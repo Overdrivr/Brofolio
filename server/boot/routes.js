f9400504ba02760bf27a68fc2904381570323f24
Rémi Bèges <remi.beges@gmail.com>
2015-12-09 14:00:40 +0100

    Admin project view page

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,6 +2,9 @@ module.exports = function(app) {
   var router = app.loopback.Router();
   router.get('/', function(req, res) {
     res.render('index.jade');
+  })
+    .get('/admin', function(req, res) {
+    res.render('admin.jade');
   });
   app.use(router);
 }
