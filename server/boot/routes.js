347acdfac1b4b1de59fa5dffc4f6567fd8bbd676
Rémi Bèges <remi.beges@gmail.com>
2015-12-13 23:57:15 +0100

    Customized routes to serve html files

---
+++ server/boot/routes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = function(app) {
+  var router = app.loopback.Router();
+  var options = {
+    root: path.join(__dirname,"/../../", app.get("views")),
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  router.get('/', function(req, res) {
+    res.sendFile("index.html",options);
+  }).get('/login', function(req, res){
+    res.sendFile("login.html",options);
+  });
+
+  app.use(router);
+}
