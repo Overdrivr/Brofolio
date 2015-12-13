20ec924c94d2f7f655733b0d3ebea7db1dfe2489
Rémi Bèges <remi.beges@gmail.com>
2015-12-13 22:15:59 +0100

    Moved app.js

---
+++ client/app/app.js
@@ -0,0 +1,62 @@
+(function(){
+  var app = angular.module("brofolioApp",["navBar"]);
+
+  app.controller("GalleryController",function(){
+    this.assets = assets;
+  });
+})();
+
+var assets = [
+  {
+    title: "Poches",
+    description: "Des poches, en veux tu, en voila.",
+    images: {
+      square:"/assets/img/12/Pouches_square.jpg",
+      thumbnails:[
+        "/assets/img/12/Pouches_thumbs.jpg",
+      ],
+      all:[
+        "/assets/img/12/Pouches.jpg"
+      ]
+    }
+  },
+  {
+    title: "Bruleur",
+    description: "Attention les doigts.",
+    images: {
+      square:"/assets/img/16/Bruleur_square.png",
+      thumbnails:[
+        "/assets/img/16/Bruleur_thumbs.jpg",
+      ],
+      all:[
+        "/assets/img/16/Bruleur.jpg"
+      ]
+    }
+  },
+  {
+    title: "Gas can",
+    description: "Pour garder le gasoil dans un endroit sur.",
+    images: {
+      square:"/assets/img/07/Gas_square.png",
+      thumbnails:[
+        "/assets/img/07/Gas_thumbs.jpg",
+      ],
+      all:[
+        "/assets/img/07/Gas.jpg"
+      ]
+    }
+  },
+  {
+    title: "Pilote",
+    description: "Un bon gaillard.",
+    images: {
+      square:"/assets/img/01/Pilot_square.png",
+      thumbnails:[
+        "/assets/img/07/Pilot_thumbs.jpg",
+      ],
+      all:[
+        "/assets/img/07/Pilot.jpg"
+      ]
+    }
+  },
+];
