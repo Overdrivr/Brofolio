487363d06e20b07a7a861b4b0d6ee3b3641d1f52
Rémi Bèges <remi.beges@gmail.com>
2015-12-16 10:28:25 +0100

    Improved ```projects``` service to edit project data
    + added controller dedicated for edition

--- client/app/app.js
+++ client/app/app.js
@@ -14,19 +14,19 @@
     .state('admin', {
       url: "/projects",
       templateUrl: "/app/authentication/project-list.html",
-      controller: "projectListController",
+      controller: "projectCreationController",
       controllerAs: "projects"
     })
     .state('create', {
       url: "/create",
       templateUrl: "/app/authentication/project-edit.html",
-      controller: "projectListController",
+      controller: "projectCreationController",
       controllerAs: "projects"
     })
     .state('edit', {
       url: "/edit/:id",
       templateUrl: "/app/authentication/project-edit.html",
-      controller: "projectListController",
+      controller: "projectEditionController",
       controllerAs: "projects"
     });
   });
@@ -42,13 +42,31 @@
     this.assets = assets;
   });
 
-  app.factory("projects",function(){
+  app.factory("projects",["$log",function($log){
     var projects = {};
-
+    projects.projectId = 0;
     projects.list = [];
 
     projects.add = function(projectData){
-      projects.list.push({title: projectData.title, description: projectData.description, assets: projectData.assets})
+      projects.list.push({
+        id: projects.projectId,
+        title: projectData.title,
+        description: projectData.description,
+        assets: projectData.assets});
+      projects.projectId++;
+    };
+
+    projects.edit = function(id,projectData){
+      // TODO : Check inputs
+      for(var i = 0 ; i < projects.list.length ; i++){
+        if(projects.list[i].id == id){
+          projects.list[i].title = projectData.title;
+          projects.list[i].description = projectData.description;
+          projects.list[i].assets = projectData.assets;
+          return;
+        }
+      }
+      $log.warn('id ', id,' not found for edition.');
     };
 
     projects.add({title: "Gas can", description: "A can of gas.", assets: ["a.jpg","b.jpg"]});
@@ -56,16 +74,32 @@
     projects.add({title: "Flamethrower", description: "A cool instant barbecue flame throwing machine.", assets: ["a.jpg","b.jpg"]});
 
     return projects;
-  });
+  }]);
 
-  app.controller("projectListController",["projects","$state",function(projects,$state){
+  app.controller("projectCreationController",["projects","$state","$log",function(projects,$state,$log){
     var self = this;
     self.projectList = projects.list;
-    self.newProject = {};
+    self.data = {};
 
-    this.addProject = function(projectData){
-      projects.add(projectData);
-      self.newProject = {};
+    this.save = function(data){
+      projects.add(data);
+      self.data = {};
+      $state.go("admin");
+    };
+  }]);
+
+  app.controller("projectEditionController",["projects","$state","$stateParams",function(projects,$state,$stateParams){
+    var self = this;
+    self.projectList = projects.list;
+
+    // Find project in list by id
+
+    self.data = {};
+    self.data.title = "test";
+
+    this.save = function(data){
+      projects.edit(0,data);
+      self.data = {};
       $state.go("admin");
     };
   }]);
