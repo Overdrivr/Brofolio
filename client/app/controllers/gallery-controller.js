4c1a221b0ea143513d6f29d9471ca03107239717
Rémi Bèges <remi.beges@gmail.com>
2015-12-17 17:59:41 +0100

    Initializing gallery with valid images

--- client/app/controllers/gallery-controller.js
+++ client/app/controllers/gallery-controller.js
@@ -1,3 +1,13 @@
 angular.module("brofolioApp").controller("GalleryController",["projects",function(projectService){
+  var self = this;
   this.projects = projectService.list;
+  self.displayables = [];
+
+  for(var i = 0 ; i < this.projects.length ; i++){
+    if(this.projects[i].assets.length){
+      this.displayables.push(this.projects[i].assets[0]);
+    }
+  }
+
+  console.log(self.displayables);
 }]);
