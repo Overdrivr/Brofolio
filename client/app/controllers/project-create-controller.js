d86d87f04ccc40dbb4408292e71401eaa342c436
Rémi Bèges <remi.beges@gmail.com>
2015-12-22 11:11:33 +0100

    Dialog can be opened, validated or cancelled

--- client/app/controllers/project-create-controller.js
+++ client/app/controllers/project-create-controller.js
@@ -1,4 +1,8 @@
-angular.module("brofolioApp").controller("projectCreationController",["projects","$state","$log",function(projects,$state,$log){
+angular.module("brofolioApp")
+  .controller("projectCreationController",
+  ["projects","$state","$log","$scope","$mdMedia","$mdDialog",
+  function(projects,$state,$log,$scope,$mdMedia,$mdDialog){
+
   var self = this;
   self.projectList = projects.list;
   self.data = {};
@@ -8,4 +12,38 @@ angular.module("brofolioApp").controller("projectCreationController",["projects"
     self.data = {};
     $state.go("admin");
   };
+
+  $scope.showCreateProjectDialog = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '/app/templates/dialog-create-project.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+  };
 }]);
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
