ae28b57727a5b40a381a2c6296fcc72dc6ce63e9
Rémi Bèges <remi.beges@gmail.com>
2015-12-28 10:59:54 +0100

    Disabled all User methods except `login` and `logout`

--- server/boot/user.js
+++ server/boot/user.js
@@ -1,6 +1,30 @@
 module.exports = function(app, cb){
   var User = app.models.User;
 
+  User.disableRemoteMethod("create", true);
+  User.disableRemoteMethod("upsert", true);
+  User.disableRemoteMethod("updateAll", true);
+  User.disableRemoteMethod("updateAttributes", false);
+
+  User.disableRemoteMethod("find", true);
+  User.disableRemoteMethod("findById", true);
+  User.disableRemoteMethod("findOne", true);
+
+  User.disableRemoteMethod("deleteById", true);
+
+  User.disableRemoteMethod("confirm", true);
+  User.disableRemoteMethod("count", true);
+  User.disableRemoteMethod("exists", true);
+  User.disableRemoteMethod("resetPassword", true);
+
+  User.disableRemoteMethod('__count__accessTokens', false);
+  User.disableRemoteMethod('__create__accessTokens', false);
+  User.disableRemoteMethod('__delete__accessTokens', false);
+  User.disableRemoteMethod('__destroyById__accessTokens', false);
+  User.disableRemoteMethod('__findById__accessTokens', false);
+  User.disableRemoteMethod('__get__accessTokens', false);
+  User.disableRemoteMethod('__updateById__accessTokens', false);
+
   User.create({email: `a@foo.fr`, password: `abc`},function(err,user){
     if(err){
       console.log(err);
