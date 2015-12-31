1f21311f77cfb01b251cfbe084c68acd0bcd3e55
Rémi Bèges <remi.beges@gmail.com>
2015-12-31 13:06:35 +0100

    extended authenticated test to project creation

--- test/test-project-authenticated.js
+++ test/test-project-authenticated.js
@@ -2,6 +2,7 @@ var request = require('supertest');
 var assert = require('chai').assert;
 var app = require('./setup-test-server.js');
 var credentials = require('../server/credentials.example.json').admin;
+var fs = require('fs');
 
 function json(verb, url) {
     return request(app)[verb](url)
@@ -11,7 +12,10 @@ function json(verb, url) {
   };
 
 describe("Unauthenticated user",function() {
+
   var accessToken;
+  var createdProjetId = -1;
+
   it('should be allowed, with valid credentials, to login and get the token', function(done){
     json('post', '/api/Users/login')
       .send(credentials)
@@ -24,4 +28,23 @@ describe("Unauthenticated user",function() {
         done();
       });
   });
+
+  it("should be able to create a new project", function(done){
+    json('post', '/api/Projects/?access_token=' + accessToken)
+      .send({
+        title:"testProject"
+      })
+      .expect(200, function(err, res){
+        if (err) return  done(err);
+        createdProjetId = res.body.id;
+        done();
+      });
+  });
+
+  it("should have created a folder in storage for the new project", function(done){
+    fs.access('../server/storage' + createdProjetId + '/', fs.F_OK, function(err){
+      if (err) return done(err);
+      done();
+    })
+  })
 });
