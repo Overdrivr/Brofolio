f505b992d2567c514e70c7c7a4f7db4a25d12728
Rémi Bèges <remi.beges@gmail.com>
2015-12-31 14:31:59 +0100

    added reference to storage datasource

--- common/models/project.js
+++ common/models/project.js
@@ -4,7 +4,7 @@ module.exports = function(Project) {
     var create = Project.create;
     Project.create = function(data, cb) {
       // Create folder
-
+      var datasource = Project.app.datasources.local;
       // Resume built-in method execution
       console.log("Folder: ", data);
       return create.apply(this, [data, cb]);
