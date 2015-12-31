d4008c87c248835542a707e1ccd0b82e7a03ac19
Rémi Bèges <remi.beges@gmail.com>
2015-12-31 14:08:35 +0100

    Customizing built-in Project `create` method
    To create a storage space

--- common/models/project.js
+++ common/models/project.js
@@ -1,3 +1,13 @@
 module.exports = function(Project) {
 
+  Project.on('dataSourceAttached', function(obj){
+    var create = Project.create;
+    Project.create = function(data, cb) {
+      // Create folder
+
+      // Resume built-in method execution
+      console.log("Folder: ", data);
+      return create.apply(this, [data, cb]);
+    };
+  });
 };
