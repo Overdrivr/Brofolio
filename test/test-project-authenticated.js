a72ec23b6e2d4279b4d1de7fbdd241f817bcee22
Rémi Bèges <remi.beges@gmail.com>
2016-01-01 15:53:27 +0100

    Added deletion tests

--- test/test-project-authenticated.js
+++ test/test-project-authenticated.js
@@ -3,6 +3,7 @@ var assert = require('chai').assert;
 var app = require('./setup-test-server.js');
 var credentials = require('../server/credentials.example.json').admin;
 var fs = require('fs');
+var storagecfg = require('../server/datasources.json');
 
 function json(verb, url) {
     return request(app)[verb](url)
@@ -36,17 +37,33 @@ describe("Unauthenticated user",function() {
       })
       .expect(200, function(err, res){
         if (err) return  done(err);
-        console.log(res.headers);
-        console.log(res.body);
         createdProjetId = res.body.id;
         done();
       });
   });
 
   it("should have created a folder in storage for the new project", function(done){
-    fs.access('./server/storage/project' + createdProjetId + '/', fs.F_OK, function(err){
+    fs.access(storagecfg.local.root + '/project' + createdProjetId + '/', fs.F_OK, function(err){
       if (err) return done(err);
       done();
+    });
+  });
+
+  it("should be able to delete an existing project", function(done){
+    json('delete', '/api/Projects/?access_token' + access_token)
+    .send({
+      id:createdProjetId
     })
-  })
+    .expect(200, function(err,res){
+      if (err) return done(err);
+      done();
+    });
+  });
+
+  if("should have removed deleted project folder from storage", function(done){
+    fs.access(storagecfg.local.root + '/project' + createdProjetId + '/', fs.F_OK, function(err){
+      if(err) return done();
+      done(new Error("Folder not removed."));
+    });
+  });
 });
