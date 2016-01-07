f0e148ab371740255e17cbb95c34600a4cb8718f
Rémi Bèges <remi.beges@gmail.com>
2016-01-07 22:03:48 +0100

    Fixed tests

--- test/test-project-authenticated.js
+++ test/test-project-authenticated.js
@@ -1,13 +1,33 @@
 var request = require('supertest');
 var assert = require('chai').assert;
-var app = require('./setup-test-server.js');
+var app = require('../server/server');
 var credentials = require('../server/credentials.example.json').admin;
 var fs = require('fs');
 var storagecfg = require('../server/datasources.json');
+var loopback = require('loopback');
+
+var storagecfg = require('../server/datasources.json');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
+/* Set up the test server */
+before(function(done){
+  // Delete test projects folder recursively
+  rimraf(storagecfg.local.root,[],function(err){
+    if (err) return done(err);
+
+    fs.mkdir(storagecfg.local.root,function(err){
+      if (err) return done(err);
+      done();
+    });
+  });
+  for(i in app.models.Asset.upload)
+    console.log(i);
+});
+
 
 function json(verb, url) {
     return request(app)[verb](url)
-      .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/);
   };
@@ -19,6 +39,7 @@ describe("User",function() {
 
   it('should be allowed, with valid credentials, to login and get the token', function(done){
     json('post', '/api/Users/login')
+      .set('Content-Type', 'application/json')
       .send(credentials)
       .expect(200, function(err, res) {
         if (err)  return done(err);
@@ -32,6 +53,7 @@ describe("User",function() {
 
   it("should be able to create a new project", function(done){
     json('post', '/api/Projects/?access_token=' + accessToken)
+      .set('Content-Type', 'application/json')
       .send({
         title:"testProject"
       })
@@ -50,16 +72,14 @@ describe("User",function() {
   });
 
   it("should be able to add an asset to the new project", function(done){
-    var stream = fs.createReadStream('./test/avatar.png')
-    var req = json('post', '/api/Projects/' + createdProjetId + '/assets?access_token=' + accessToken)
-    .send({
-      title: "test",
-      resource: stream
-    })
+    //var stream = fs.createReadStream('./test/avatar.png')
+    var req = json('post', '/api/Assets/upload?access_token='+accessToken)
+    .set('Content-Type', 'multipart/form-data')
+    .attach('image','./test/avatar.png')
+    .field('extra_info', '{"name":"booya"}')
     .expect(200);
 
     req.end(function(err, res){
-      console.log(err);
       if (err) return done(err);
       done();
     });
@@ -73,7 +93,8 @@ describe("User",function() {
   });
 
   it("should be able to delete an existing project", function(done){
-    json('delete', '/api/Projects/' + createdProjetId + '?access_token=' + accessToken)
+    json('delete', '/api/Projects/project' + createdProjetId + '?access_token=' + accessToken)
+    .set('Content-Type', 'application/json')
     .expect(200, function(err,res){
       if (err) return done(err);
       done();
