0ceeb0fde17abecf425174f77ffa8518c5186136
Rémi Bèges <remi.beges@gmail.com>
2016-01-24 11:59:52 +0100

    login-controller now uses lbServices

--- client/app/controllers/login-controller.js
+++ client/app/controllers/login-controller.js
@@ -1,4 +1,19 @@
-(function(){
-   var app = angular.module("Auth", []);
+angular.module("brofolioApp")
+  .controller('LoginCtrl',function($scope, User){
+    $scope.credentials = {
+      email: '',
+      password: ''
+    };
 
-})();
+    $scope.login = function(credentials) {
+      $scope.loginResult = User.login(credentials,
+      function() {
+        // success
+        console.log("Logged in")
+      },
+      function(res) {
+        // failure
+        console.log("Failed to log :",res)
+      });
+    }
+  });
