ca8260d5fc40ce8a357b1182c691390dbf5764c7
Rémi Bèges <remi.beges@gmail.com>
2016-03-13 20:33:24 +0100

    Testing asset upload + front page refresh

--- client/test/spec.js
+++ client/test/spec.js
@@ -16,13 +16,45 @@ describe('Brofolio', function() {
 
   it('should initally see no project',
   function() {
-      expect(element.all(by.css('md-list')).count()).toEqual(0);
+      e = element(by.id('projectList')).all(by.css("md-list-item"))
+      expect(e.count()).toEqual(0);
   });
 
-  it('then should create a project called dummy',
+  it('should create a project called dummy',
   function() {
       browser.findElement(by.css('button')).click();
       browser.findElement(by.model('title')).sendKeys('dummy');
       browser.findElement(by.buttonText('Accept')).click();
+  });
+
+  it('should see the freshly created project inside project list.',
+  function() {
+      e = element(by.id('projectList')).all(by.css("md-list-item"))
+      expect(e.count()).toEqual(1);
+  });
+
+  it('should go to the newly created project admin page',
+  function() {
+    browser.findElement(by.css('a[href*="#/edit/1"]')).click()
+  });
+
+  var path = require('path');
+
+  it('should upload an asset to the project',
+  function() {
+    var fileToUpload = './test/avatar.png'
+    var absolutePath = path.resolve(__dirname, fileToUpload);
+    $('input[type="file"]').sendKeys(absolutePath);
+    $('#uploadButton').click();
+  });
+
+  it('should go back to the main page and see the newly created asset',
+  function() {
+    browser.get('#');
+    var c = element.all(by.css('img')).count();
+    expect(c).toEqual(1);
+
+    var e = browser.findElement(by.css('img'))
+    expect(e.getAttribute('ng-src')).toEqual('avatar.png')
   });
 });
