d0871458b6216a9e02e59794ff128f1251ec8a22
Rémi Bèges <remi.beges@gmail.com>
2016-03-13 21:50:31 +0100

    Removing hard-coded data in favor of loopback-services
    For gallery controller

--- client/app/controllers/gallery-controller.js
+++ client/app/controllers/gallery-controller.js
@@ -1,34 +1,14 @@
-angular.module("brofolioApp").controller("GalleryController",["projects",function(projectService){
+angular.module("brofolioApp").controller("GalleryController",["Project",function(Project){
   var self = this;
-  //this.projects = projectService.list;
-  self.displayables = images;
-  self.thumbsleft = [];
-  self.thumbsright = [];
+  self.displayables = [];
 
-  for(var i = 0 ; i < 5 ; i++){
-    self.thumbsleft.push(images[0].thumb);
-  }
-
-  for(var i = 0 ; i < 5 ; i++){
-    self.thumbsright.push(images[0].thumb);
-  }
+  // Get project list with limit of ten
+  Project.find("[where][id][lt]=10",
+    function(value,responseHeaders){
+      console.log(value);
+      self.displayables = value;
+    },
+    function(httpResponse){
+      console.error(httpResponse);
+    });
 }]);
-
-var images = [
-  {
-    title: "WWII Pilot",
-    description: "My first attempt to digital painting. Done in two weeks using Photoshop. The face of the pilot is inspired from a friend of mine.",
-    full: "assets/img/01/Pilote.jpg",
-    link: "assets/img/01/Pilot_square.png",
-    thumb: "assets/img/01/Pilot_thumbnail.png"
-  },
-  {
-    link: "assets/img/07/Gas_square.png"
-  },
-  {
-    link: "assets/img/12/Pouches_square.jpg"
-  },
-  {
-    link: "assets/img/16/Bruleur_square.png"
-  }
-]
